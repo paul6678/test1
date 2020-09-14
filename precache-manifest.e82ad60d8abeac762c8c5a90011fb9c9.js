43d99a09d0c6aba5b301471e30ad8ca162124bda
paul6678 <paulpaul6678@gmail.com>
2020-09-14 11:14:34 +0800

    gh-page

---
+++ precache-manifest.e82ad60d8abeac762c8c5a90011fb9c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbda7613071c66266d026e8a7498d37",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "31670039028b9771dfac",
+    "url": "/test1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/test1/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test1/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31670039028b9771dfac",
+    "url": "/test1/static/js/main.d9146084.chunk.js"
+  },
+  {
+    "revision": "b15592ac57cc00ee0eed",
+    "url": "/test1/static/js/runtime-main.1e612a43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
